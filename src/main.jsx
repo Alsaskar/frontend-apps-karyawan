1804a3d325b50b3de099e33154a767a073d65c98
Rizal Kadamong <rizallkadamong@gmail.com>
2023-07-16 17:33:14 +0800

    Update halaman login

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,8 @@
 import ReactDOM from 'react-dom/client';
 import Router from './Router.jsx';
 import { BrowserRouter } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './assets/scss/main.scss';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
